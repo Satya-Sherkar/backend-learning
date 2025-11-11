94bce353e12483a7e98294a808db69129b7ac10b
satyamsherkar <satyasherkar@gmail.com>
2025-11-11 16:01:22 +0530

    add login and logout user controller with auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { ApiError } from "../utils/apiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+  
+    if (!token) {
+      throw new ApiError(401, "Not authorized, token missing");
+      }
+      
+      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findById(decodedToken.id).select("-password -refreshToken");
+  
+      if (!user) {
+        throw new ApiError(401, "Invalid access token, user not found");
+      }
+  
+      req.user = user;
+      next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token")
+  }
+
+
+});

e9db51912f10e2adda12a28f8139b0d5a9dcfa51
Satyam Sherkar <satyasherkar@gmail.com>
2025-11-15 19:17:13 +0530

    Refactor API response structure in changeCurrentPassword and getCurrentUser functions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -224,13 +224,13 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .json(new ApiResponse(200, "Password changed successfully"));
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "Current user fetched succesfully");
+    .json(new ApiResponse(200, req.user, "Current user fetched succesfully"));
 });
 
 const updateUserDetails = asyncHandler(async (req, res) => {
